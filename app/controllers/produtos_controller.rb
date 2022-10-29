7089a0ee847deb448de2ca8c530a8ebae35fc15a
Daniel Fonseca da Silva <developer-web@programmer.net>
2022-10-29 02:15:03 -0300

    feat: Produtos controller

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -1,2 +1,6 @@
 class ProdutosController < ApplicationController
+    def index
+        @produtos = Produto.order(nome: :desc).limit 2
+        @produto_com_desconto = Produto.order(:preco).limit 1
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  get "produtos", to: "produtos#index"
+  root to: "produtos#index"
 end
