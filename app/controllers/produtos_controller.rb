a2f63606e462d7c1bc42cdae35c6e0f385318738
Daniel Fonseca da Silva <developer-web@programmer.net>
2022-11-02 01:48:35 -0300

    feat: Criando a busca

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -1,6 +1,6 @@
 class ProdutosController < ApplicationController
     def index
-        @produtos = Produto.order(nome: :desc).limit 10
+        @produtos = Produto.order(nome: :asc).limit 10
         @produto_com_desconto = Produto.order(:preco).limit 1
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "produtos/busca", to: "produtos#busca", as: :busca_produto
   resources :produtos, only: [:new, :create, :destroy]
   root to: "produtos#index"
 end
