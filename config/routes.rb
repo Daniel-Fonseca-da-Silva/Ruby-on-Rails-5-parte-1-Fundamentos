2e13579d5a9d719d7e184428c030b4d1252491b0
Daniel Fonseca da Silva <developer-web@programmer.net>
2022-11-02 00:57:13 -0300

    feat: Antes de deletar e resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  delete "produtos/:id", to: "produtos#destroy", as: :produto
-  post "produtos", to: "produtos#create"
-  get "produtos/new", to: "produtos#new"
+  resources :produtos, only: [:new, :create, :destroy]
   root to: "produtos#index"
 end
